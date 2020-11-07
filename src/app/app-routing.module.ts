1e6b03485fd048bd4e79fc980b1f103a82e27ed7
xcorr <onielca2@gmail.com>
2020-11-06 22:19:01 -0400

    caleb

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,11 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule) },
-  { path: 'signup', loadChildren: () => import('./signup/signup.module').then(m => m.SignupPageModule) }
+  { path: 'signup', loadChildren: () => import('./signup/signup.module').then(m => m.SignupPageModule) },  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  }
+
 ];
 
 @NgModule({
